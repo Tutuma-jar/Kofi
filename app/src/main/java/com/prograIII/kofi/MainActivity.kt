7d82624788817ca5f64136713563c1ffecb6e4a5
Diego Loayza <dloayzarr@gmail.com>
2025-12-10 21:44:27 -0400

    primera versión menu

--- app/src/main/java/com/prograIII/kofi/MainActivity.kt
+++ app/src/main/java/com/prograIII/kofi/MainActivity.kt
@@ -2,10 +2,37 @@ package com.prograIII.kofi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var btnMenu: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        btnMenu = findViewById(R.id.btnMenu) // Este es el botón dentro del Toolbar
+
+        // Al hacer click en el icono de menú
+        btnMenu.setOnClickListener {
+            // Abrimos el drawer desde el lado derecho (END)
+            if (!drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                drawerLayout.openDrawer(GravityCompat.END)
+            } else {
+                drawerLayout.closeDrawer(GravityCompat.END)
+            }
+        }
+    }
+
+    // Manejar el botón "Atrás" para cerrar el drawer si está abierto
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
